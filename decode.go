6dfc030d44b276e6030548db0e11829632c6157e
Nils Hasenbanck <nils@hasenbanck.de>
2012-12-05 21:49:30 +0100

    Fixed a build error and the doc.
    Signed-off-by: Nils Hasenbanck <nils@hasenbanck.de>

--- decode.go
+++ decode.go
@@ -4,13 +4,13 @@ package nwa
 
 import "io"
 
-// Decode returns the encoded sound wave as a slice in WAVE format.
+// Decode returns the dencoded sound as a slice in WAVE format.
 func DecodeAsWav(r io.Reader)  (buffer []byte, err error) {
 	return nil, nil
 }
 
-// DecoeKoeAsWav returns the decoded sound file at the given offset/length
+// DecoeKoeAsWav returns the decoded sound at the given offset/length
 // from a KOE file as a slice in WAVE format.
-func DecodeKoeAsWav(r io.Reader, offset int, length int) (buffer []byte, err error]) {
+func DecodeKoeAsWav(r io.Reader, offset int, length int) (buffer []byte, err error) {
 	return nil, nil
 }
