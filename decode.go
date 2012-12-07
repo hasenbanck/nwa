c88e7f3d25aaeed5c2645657b194bee3e33b15f5
Nils Hasenbanck <nils@hasenbanck.de>
2012-12-07 21:45:56 +0100

    Implemented ReadHeader and CheckHeader of NwaData.
    I couldn't test everything yet and the original comments are still
    in japanese. There seem also to be some typos in the source code,
    which I need to test later.
    
    Signed-off-by: Nils Hasenbanck <nils@hasenbanck.de>

--- decode.go
+++ decode.go
@@ -8,21 +8,15 @@ import (
 
 // Decode returns the dencoded sound as a slice in WAVE format.
 func DecodeAsWav(r io.Reader) ([]byte, error) {
+	data, err := NewNwaData(r)
+	if err = data.ReadHeader(); err != nil {
+		return nil, err
+	}
+	if err = data.CheckHeader(); err != nil {
+		return nil, err
+	}
 	//var bs int
-	var d nwaData
-	var err error
-
-	if err = d.OpenReader(r); err != nil {
-		return nil, err
-	}
-	if err = d.ReadHeader(); err != nil {
-		return nil, err
-	}
-	if err = d.CheckHeader(); err != nil {
-		return nil, err
-	}
 	//if bs, err = d.BlockLength(); err != nil {return nil, err}
-
 	/**
 	data := make([]byte, bs)
 	while( (read, err = h.Decode(data, skip_count)); read != 0) {
--- nwadata.go
+++ nwadata.go
@@ -1,49 +1,148 @@
 package nwa
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
+	"fmt"
 	"io"
 )
 
-type nwaData struct {
-	Channels int
-	Bps      int // bits per sample
-	Freq     int // samples per second
-	Blocks   int // block count
-	Datasize int // all data size
-
-	reader       io.Reader // Reader to read the NWA data from
-	complevel    int       // compression level
-	use_unlength int       // run length encoding
-	compdatasize int       // compressed data size
-	samplecount  int       // all samples
-	blocksize    int       // samples per block
-	restsize     int       // samples of the last block
-	dummy2       int       // ? : 0x89
-	curblock     int
-	offsets      []int
-	offset_start int
-	filesize     int
-	tmpdata      []byte
+type NwaData struct {
+	reader        io.Reader // Reader to read the NWA data from
+	channels      int16     // channels
+	bps           int16     // bits per sample
+	freq          int32     // samples per second
+	blocks        int32     // block count
+	datasize      int32     // all data size
+	complevel     int32     // compression level
+	use_runlength int32     // run length encoding
+	compdatasize  int32     // compressed data size
+	samplecount   int32     // all samples
+	blocksize     int32     // samples per block
+	restsize      int32     // samples of the last block
+	dummy2        int32     // ? : 0x89
+	curblock      int
+	offsets       []int32
+	filesize      int // TODO: Do we need this?
+	tmpdata       []byte
 }
 
-func (d *nwaData) OpenReader(r io.Reader) error {
+func NewNwaData(r io.Reader) (*NwaData, error) {
 	if r == nil {
-		return errors.New("io: Reader is nil.")
+		return nil, errors.New("Reader is nil.")
 	}
-	d.reader = r
+	return &NwaData{reader: r}, nil
+}
+
+func (d *NwaData) ReadHeader() error {
+	header := make([]byte, 0x2c)
+	d.curblock = -1
+
+	if count, err := d.reader.Read(header); count != 0x2c || err != nil {
+		if err == nil {
+			err = errors.New("Can't read the header")
+		}
+		return err
+	}
+
+	buffer := bytes.NewBuffer(header)
+	binary.Read(buffer, binary.LittleEndian, &d.channels)
+	binary.Read(buffer, binary.LittleEndian, &d.bps)
+	binary.Read(buffer, binary.LittleEndian, &d.freq)
+	binary.Read(buffer, binary.LittleEndian, &d.complevel)
+	binary.Read(buffer, binary.LittleEndian, &d.use_runlength)
+	binary.Read(buffer, binary.LittleEndian, &d.blocks)
+	binary.Read(buffer, binary.LittleEndian, &d.datasize)
+	binary.Read(buffer, binary.LittleEndian, &d.compdatasize)
+	binary.Read(buffer, binary.LittleEndian, &d.samplecount)
+	binary.Read(buffer, binary.LittleEndian, &d.blocksize)
+	binary.Read(buffer, binary.LittleEndian, &d.restsize)
+	binary.Read(buffer, binary.LittleEndian, &d.dummy2)
+
+	if d.complevel == -1 { // 無圧縮rawデータ
+		// 適当に決め打ちする
+		d.blocksize = 65536
+		d.restsize = (d.datasize % (d.blocksize * (int32)(d.bps/8))) / (int32)(d.bps/8)
+		// Todo: Can we make the following 3 lines better to read?
+		var rest int32
+		if d.restsize > 0 {
+			rest = 1
+		}
+		d.blocks = d.datasize/(d.blocksize*(int32)(d.bps/8)) + rest
+	}
+	if d.blocks <= 0 || d.blocks > 1000000 {
+		// １時間を超える曲ってのはないでしょ
+		return fmt.Errorf("Blocks are too large: %d\n", d.blocks)
+	}
+
+	// TODO: Do we need the filesize? io.Reader doesn't provide such a thing
+
+	if d.complevel == -1 {
+		return nil
+	}
+
+	// Read the offset indexes
+	// TODO: Test me!
+	d.offsets = make([]int32, d.blocks)
+	offsetData := make([]byte, d.blocks*4)
+	if count, err := d.reader.Read(offsetData); count != (int)(d.blocks*4) || err != nil {
+		if err == nil {
+			err = errors.New("Can't read the offset block'")
+		}
+		return err
+	}
+	buffer = bytes.NewBuffer(offsetData)
+	var i int32
+	for i = 0; i < d.blocks; i++ {
+		binary.Read(buffer, binary.LittleEndian, &d.offsets[i])
+	}
 	return nil
 }
 
-func (d *nwaData) ReadHeader() error {
-	return errors.New("Not implemented yet")
+func (d *NwaData) CheckHeader() error {
+	if d.complevel != -1 && d.offsets == nil {
+		return nil
+	}
+	if d.channels != 1 && d.channels != 2 {
+		return fmt.Errorf("This library only supports mono / stereo data: data has %d channels\n", d.channels)
+	}
+	if d.bps != 8 && d.bps != 16 {
+		return fmt.Errorf("This library only supports 8 / 16bit data: data is %d bits\n", d.bps)
+	}
+	if d.complevel == -1 {
+		var byps int32 = (int32)(d.bps / 8) // bytes per sample
+		if d.datasize != d.samplecount*byps {
+			return fmt.Errorf("Invalid datasize: datasize %d != samplecount %d * samplesize %d\n", d.datasize, d.samplecount, byps)
+		}
+		if d.samplecount != (d.blocks-1)*d.blocksize+d.restsize {
+			return fmt.Errorf("Total sample count is invalid: samplecount %d != %d*%d+%d(block*blocksize+lastblocksize)\n", d.samplecount, d.blocks-1, d.blocksize, d.restsize)
+		}
+		return nil
+	}
+	// TODO: complevel > 5????
+	// if (complevel < 0 || complevel > 2) {
+	if d.complevel < 0 || d.complevel > 5 {
+		return fmt.Errorf("This library supports only compression level -1,0,1,2: the compression level of the data is %d\n", d.complevel)
+	}
+
+	// TODO: If we NEED the filesize, check it here!
+
+	if d.offsets[d.blocks-1] >= d.compdatasize {
+		return fmt.Errorf("The last offset overruns the file.\n")
+	}
+	var byps int32 = (int32)(d.bps / 8) // bytes per sample
+	if d.datasize != d.samplecount*byps {
+		return fmt.Errorf("Invalid datasize: datasize %d != samplecount %d * samplesize %d\n", d.datasize, d.samplecount, byps)
+	}
+	if d.samplecount != (d.blocks-1)*d.blocksize+d.restsize {
+		return fmt.Errorf("Total sample count is invalid: samplecount %d != %d*%d+%d(block*blocksize+lastblocksize).\n", d.samplecount, d.blocks-1, d.blocksize, d.restsize)
+	}
+	d.tmpdata = make([]byte, d.blocksize*byps*2) // これ以上の大きさはないだろう、、、
+	return nil
 }
 
-func (d *nwaData) CheckHeader() error {
-	return errors.New("Not implemented yet")
-}
-
-func (d *nwaData) BlockLength() (int, error) {
+func (d *NwaData) BlockLength() (int32, error) {
 	if d.complevel != -1 {
 		if d.offsets == nil {
 			return 0, errors.New("BlockLength could not be calculcated: No offsets set!")
@@ -52,7 +151,7 @@ func (d *nwaData) BlockLength() (int, error) {
 			return 0, errors.New("BlockLength could not be calculcated: No data slice set!")
 		}
 	}
-	return d.blocksize * (d.Bps / 8), nil
+	return d.blocksize * (int32)(d.bps/8), nil
 }
 
 /* Original comment of the function:
@@ -60,6 +159,6 @@ func (d *nwaData) BlockLength() (int, error) {
 ** 返り値は作成したデータの長さ。終了時は 0。
 ** エラー時は -1
  */
-func (d *nwaData) Decode(data []byte, skipcount int) (int, error) {
+func (d *NwaData) Decode(data []byte, skipcount int) (int, error) {
 	return 0, errors.New("Not implemented yet")
 }
